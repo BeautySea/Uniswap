10d08b2c6f7bc04872ff633ceee6b4ef01967220
BeautySea <benjaminlin53@outlook.com>
2024-09-14 12:51:30 +0300

    vite tailwind typescript react_

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { ReactElement } from 'react';
+import {BrowserRouter as Router, Navigate , Routes, Route} from 'react-router-dom';
+import './App.css';
+import Landing from './pages/Landing';
+// const PrivateRoute = ({element}:{element:ReactElement}) => (
+//   const token = localStorage.getItem('authToken');
+//   return token? element: <Navigate to="login"/>
+// )
+// const AuthenticatedRoute = ({element}:{element:ReactElement})=> (
+//   // get the authToken from the localstorage
+//   const token = localStorage.getItem('authToken');
+//   return token? <Navigate to="/dashboard"/>: element;
+// )  
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,8 @@
-import { ReactElement } from 'react';
-import {BrowserRouter as Router, Navigate , Routes, Route} from 'react-router-dom';
-import './App.css';
-import Landing from './pages/Landing';
-// const PrivateRoute = ({element}:{element:ReactElement}) => (
-//   const token = localStorage.getItem('authToken');
-//   return token? element: <Navigate to="login"/>
-// )
-// const AuthenticatedRoute = ({element}:{element:ReactElement})=> (
-//   // get the authToken from the localstorage
-//   const token = localStorage.getItem('authToken');
-//   return token? <Navigate to="/dashboard"/>: element;
-// )  
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing/>} />
-      </Routes>
-    </Router>
-  );
+    <div className="w-full h-screen bg-blue-800 flex justify-center items-center">
+      <div className="h-20 bg-slate-600 px-10 flex items-center"><h1>Hello, TailwindCSS + Vite + Typescript +  React </h1></div>
+    </div>
+  )
 }
-export default App;
+export default App
